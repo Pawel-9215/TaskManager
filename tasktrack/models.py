03aef3a233f222309ac55da423e910b8ef534c6d
Pawel-9215 <hordyniak@gmail.com>
2021-05-24 21:12:13 +0200

    FirstCommit

---
+++ tasktrack/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
+from django.utils import timezone
+
+# Create your models here.
+STATUS = (
+    (0, "ToDo"),
+    (1, "Done")
+)
+
+class Project(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=24, unique=True)
+    status = models.IntegerField(choices=STATUS, default = 0)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Task(models.Model):
+    
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    project = models.ForeignKey('tasktrack.Project', related_name='Tasks', null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    description = models.TextField()
+    created_on = DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default = 0)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self) -> str:
+        return self.title
+
+    def update(self):
+        self.updated_on = timezone.now()
+        self.save()
+
+    
---
+++ tasktrack/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Project, STATUS, Task
+from django.contrib.auth.mixins import LoginRequiredMixin
+from tasktrack.forms import TaskForm, ProjectForm
+
+# Create your views here.
+
+class TaskList(LoginRequiredMixin, generic.ListView):
+    login_url = '/login/'
+    model = Task
+    queryset = Task.objects.filter(status=0).order_by('created_on')
+    template_name = "task_list.html"
+
+class AboutView(generic.TemplateView):
+    template_name = "about.html"
+
+class TaskListDone(LoginRequiredMixin, generic.ListView):
+    login_url = '/login/'
+    model = Task
+    queryset = Task.objects.filter(status=1).order_by('created_on')
+    template_name = "done_tasks.html"
+
+class CreateTaskView(LoginRequiredMixin, generic.CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'task_list.html'
+
+    form_class = TaskForm
+    model = Task
+
+class CreateTaskView(LoginRequiredMixin, generic.CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'task_list.html'
+
+    form_class = ProjectForm
+    model = Project
